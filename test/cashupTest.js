eb3807bea1830e007135df2769868bdb05b0783e
Nikita Hovratov <nikita.h@live.de>
2017-05-15 16:13:39 +0100

    Add tests

---
+++ test/cashupTest.js
@@ -0,0 +1,39 @@
+var assert = chai.assert;
+var api = cashup2.api;
+var Person = api.Person;
+var Amount = api.Amount;
+
+describe("Person", function() {
+  describe("constructor", function() {
+    it("should have the constructor Person", function() {
+      var person = new Person("Nikita");
+      var proto = Object.getPrototypeOf(person);
+      assert(proto.constructor === Person, "Person is not the constructor");
+    });
+  });
+  describe("addAmount", function() {
+    it("should add a Amount to the list", function() {
+      var person = new Person("Nikita");
+      var amount = new Amount();
+      person.addAmount(amount);
+      assert(person.amounts.length === 1, "No Amount added to the list");
+    });
+  });
+});
+
+describe("Amount", function() {
+  describe("constructor", function() {
+    it("should have the constructor Amount", function() {
+      var amount = new Amount();
+      var proto = Object.getPrototypeOf(amount);
+      assert(proto.constructor === Amount, "Amount is not the constructor");
+    });
+  });
+  describe("setValue", function() {
+    it("should set a value to the Amount", function() {
+      var amount = new Amount();
+      amount.setValue(1.11);
+      assert(typeof amount.value === "number", "Amount is not a number");
+    });
+  });
+});
